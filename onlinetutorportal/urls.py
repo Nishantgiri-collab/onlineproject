5d0029dda68be742b5dfa4cf528303e2552d182f
Nishantgiri-collab <nshntgiri@gmail.com>
2025-09-26 22:21:49 +0530

    Added sitemap support and sitemap.xml endpoint

--- onlinetutorportal/urls.py
+++ onlinetutorportal/urls.py
@@ -2,11 +2,21 @@
 from django.contrib import admin
 from django.urls import path
 from tutorapp import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
 
+sitemaps_dict = {
+    "static": StaticViewSitemap,
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('registration/', views.register_user, name='registration'),
     path('login/', views.login_user, name='login'),
     path("book/", views.book_class, name="book_class"),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps_dict}, name="sitemap"),
 ]
+
+
+
+
